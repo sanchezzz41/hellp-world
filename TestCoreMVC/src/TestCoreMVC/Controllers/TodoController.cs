df45faff42e7f77b2578ae90ecc9e9963513d474
sanchezzz41 <sanchezzz41@mail.ru>
2017-07-12 19:15:25 +0400

    Первые

---
+++ TestCoreMVC/src/TestCoreMVC/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestCoreMVC.Model;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestCoreMVC.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet]
+        [Route("[controller]/[action]/{id}")]
+        public ActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
---
+++ TestCoreMVC/src/TestCoreMVC/Model/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestCoreMVC.Model
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+      
+    }
+}
